8fa70251db773e59d8b7954009bd7f325d48497e
chayan chowdhury <chayanchowdhury43@yahoo.com>
2024-03-22 23:57:39 +0530

    Initial commit

---
+++ app/src/main/java/com/example/datastore_jetpackcompose_mvvm_daggerhilt/ui/theme/Theme.kt
@@ -0,0 +1,28 @@
+package com.example.datastore_jetpackcompose_mvvm_daggerhilt.ui.theme
+
+import android.app.Activity
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
+
+@Composable
+fun DataStorejetpackComposeMVVMDaggerhiltTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    // Dynamic color is available on Android 12+
+    dynamicColor: Boolean = true,
+    content: @Composable () -> Unit
+) {
+
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+        }
+    }
+}
